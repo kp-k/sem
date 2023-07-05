6646421bc2b32f65c21bffc708bfe075dc4a9460
40614804 <40614804@live.napier.ac.uk>
2023-07-05 15:54:43 +0630

    connect to mongo dbserver

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,16 +9,16 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
+        Document doc = new Document("name", "Kyi Phyu Khin")
                 .append("class", "Software Engineering Methods")
-                .append("year", "2021")
+                .append("year", "2023")
                 .append("result", new Document("CW", 95).append("EX", 85));
         // Add document to collection
         collection.insertOne(doc);
@@ -26,5 +26,6 @@ public class App
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
+
     }
 }
